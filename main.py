b5f73f9221a710e45152a7d90fd9b88ab3b951e8
Daniel Barcos <dbarcos@google.com>
2019-11-29 14:34:51 +0100

    Selection dialog, background and size

--- main.py
+++ main.py
@@ -6,32 +6,26 @@ PyCloudMail is a WordCloud generator in Python.
 """
 
 import os
-from os import path
 from wordcloud import WordCloud
-
-# get data directory (using getcwd() is needed to support running example in generated IPython notebook)
-d = path.dirname(__file__) if "__file__" in locals() else os.getcwd()
-
-# Read the whole text.
-text = open(path.join(d, 'constitution.txt')).read()
-
-# Generate a word cloud image
-wordcloud = WordCloud().generate(text)
-# print(wordcloud)
-
-# Display the generated image:
-# the matplotlib way:
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import filedialog
+
+# Get data directory
+root = tk.Tk()
+root.withdraw()
+file_path = filedialog.askopenfilename(title="Select the source txt file", filetypes= (("TXT files","*.txt"),("All files","*.*")))
+filename = os.path.basename(file_path)
+pre, ext = os.path.splitext(file_path)
+
+# Read the whole text
+text = open(file_path).read()
+
+# Generate a word cloud image and saving it into a file
+wordcloud = WordCloud(width=2000, height=1126, background_color=None, mode="RGBA").generate(text)
+wordcloud.to_file(pre + ".png")
+
+# Display the generated image (interpolation='bilinear' to make the displayed image appear more smoothly)
 plt.imshow(wordcloud, interpolation='bilinear')
 plt.axis("off")
-
-# lower max_font_size
-wordcloud = WordCloud(max_font_size=40).generate(text)
-plt.figure()
-plt.imshow(wordcloud, interpolation="bilinear")
-plt.axis("off")
 plt.show()
-
-# The pil way (if you don't have matplotlib)
-# image = wordcloud.to_image()
-# image.show()
